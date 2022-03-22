08613503fa61c64c09bc4c287742a5c63ea764eb
Charles Phil <charlesg.phil@gmail.com>
2022-03-22 12:26:59 -0400

    Created conda environment.yml and started pymongo db

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,13 @@
 import scrape_mars
 from flask import Flask
+import pymongo
 
 app = Flask(__name__)
+
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+db = client.mars_db
+collection = db.scraped_data
 
 
 # @app.route("/")
@@ -12,7 +18,24 @@ app = Flask(__name__)
 @app.route("/scrape")
 def scrape():
     content = scrape_mars.scrape()
-    return f"<img src={content['hemispheres'][3]['url']}>"
+
+    return (
+        f"<p>{content['latest_news']['title']}</p>"
+        f"<p>{content['latest_news']['paragraph']}</p>"
+        f"<a href={content['latest_news']['url']}>Article Link</a>"
+        f"<p>{content['featured_img']['name']}</p>"
+        f"<img src={content['featured_img']['img_url']}>"
+        f"<a href={content['featured_img']['article_url']}>Article Link</a>"
+        f"{content['facts']}"
+        f"<p>{content['hemispheres'][0]['name']}</p>"
+        f"<img src={content['hemispheres'][0]['url']}>"
+        f"<p>{content['hemispheres'][1]['name']}</p>"
+        f"<img src={content['hemispheres'][1]['url']}>"
+        f"<p>{content['hemispheres'][2]['name']}</p>"
+        f"<img src={content['hemispheres'][2]['url']}>"
+        f"<p>{content['hemispheres'][3]['name']}</p>"
+        f"<img src={content['hemispheres'][3]['url']}>"
+    )
 
 
 if __name__ == "__main__":
